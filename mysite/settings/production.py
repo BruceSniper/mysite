60515b2db2bc9e9c5f75bf014b3655ef413b37b6
BruceSniper <270521009@qq.com>
2020-04-10 22:50:20 +0800

    回滚操作

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -4,7 +4,7 @@
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2pe8eih8oah2_2z1=7f84bzme7^bwuto7y&f(#@rgd9ux9mp-3'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
