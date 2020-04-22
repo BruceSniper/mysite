c8e17525ce7eeceb597b79b0771323f1fc3eba36
BruceSniper <270521009@qq.com>
2020-04-22 17:34:32 +0800

    修改后台admin框架

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -34,6 +34,7 @@ PAGINATION_SETTINGS = {
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
