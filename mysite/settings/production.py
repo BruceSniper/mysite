220682c53c6487c675aad55df4574a36d5f7656b
BruceSniper <270521009@qq.com>
2020-04-24 09:45:14 +0800

    修改路由

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -9,4 +9,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.brucecode.xyz']
+ALLOWED_HOSTS = ['www.brucecode.xyz', '114.116.232.108']
