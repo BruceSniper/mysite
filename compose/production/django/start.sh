6cb82b82a0b715f5856d232331f205ff7f50457f
BruceSniper <270521009@qq.com>
2020-07-20 20:04:26 +0800

    修改端口号

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn mysite.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+gunicorn mysite.wsgi:application -w 4 -k gthread -b 0.0.0.0:8010 --chdir=/app
